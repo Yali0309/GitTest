c76d380234b4a0d9497967b2d1434c3a6dcb075d
是风动 <48672672+Yali0309@users.noreply.github.com>
2019-09-05 16:10:31 +0800

    Update Demo.java

--- Demo.java
+++ Demo.java
@@ -1,6 +1,7 @@
 Public Class Demo(){
   Public static void main(String[] args){
-    System.out.printin("Hello word!");
+    //System.out.printin("Hello word!");
+    System.out.printin("Hello Word!");
   
   }
 
